fdbdb6d353f22b2d7c93f3dcc85967469244f24e
SuYeon <includeashe@gmail.com>
2026-02-13 10:10:32 +0900

    - Feat: Add List && ListIterator.
    - List 기존 코드에서 문제 있는 부분 수정
    - ListIterator 만들고, 개념 설명
    - 포인터를 배열로 확인할 때 유용한 debug 구문 설명 (예: list.data, 10)

--- 2.List/List.h
+++ 2.List/List.h
@@ -3,17 +3,115 @@
 #include <iostream>
 #include <cassert>
 
+// iterator.
+template<typename List>
+class ListIterator
+{
+public:
+	// Set Type Alias.
+	// List가 template parameter로 넘어온 경우 typename까지 같이 써줘야 함.
+	using ValueType = typename List::ValueType;
+	using PointerType = ValueType*;
+	using ReferenceType = ValueType&;
+
+public:
+	ListIterator(PointerType ptr)
+		:ptr(ptr)
+	{
+
+	}
+
+	// operator overloading.
+
+	// 전위 증가 연산자.
+	ListIterator& operator++()
+	{
+		++ptr;
+		return *this;
+	}
+
+	// 후위 증가 연산자.
+	ListIterator& operator++(int)
+
+	{
+		// 현재 반복자를 임시 저장.
+		ListIterator iterator = *this;
+
+		// 내부 pointer를 ++처리.
+		++(*this);
+
+		// 앞서 지정했던 반복자 반환.
+		return iterator;
+	}
+
+
+	// 전위 감소 연산자.
+	ListIterator& operator--()
+	{
+		--ptr;
+		return *this;
+	}
+
+	// 후위 감소 연산자.
+	ListIterator& operator--(int)
+
+	{
+		// 현재 반복자를 임시 저장.
+		ListIterator iterator = *this;
+
+		// 내부 pointer를 --처리.
+		--(*this);
+
+		// 앞서 지정했던 반복자 반환.
+		return iterator;
+	}
+
+	ReferenceType operator[](int index)
+	{
+		return *(ptr + index);
+	}
+
+	PointerType operator->()
+	{
+		return ptr;
+	}
+
+	ReferenceType operator*()
+	{
+		return *ptr;
+	}
+
+	// 비교 연산자 오버로딩.
+	bool operator==(const ListIterator& other) const
+	{
+		return ptr == other.ptr;
+	}
+
+	bool operator!=(const ListIterator& other) const
+	{
+		return !(*this == other);
+	}
+
+private:
+	// iterator는 결국 pointer!
+	PointerType ptr = nullptr;
+
+};
+
 // 자동으로 크기가 늘어나는 배열 (List/ Vector).
 template<typename T> 
 class List
 {
 public:
+	// Set type alias.
+	using ValueType = T;
+	using Iterator = ListIterator< List<T> >;
+
+public:
 	List()
 	{
 		// 저장 공간 할당.
-		//ReAllocate(capacity);
-		data = new T[capacity];
-		memset(data, 0, sizeof(T) * capacity);
+		ReAllocate(capacity);
 	}
 
 	~List()
@@ -35,7 +133,10 @@ public:
 		memset(newBlock, 0, sizeof(T) * newCapacity);
 
 		// 2. 기존 항목을 해로운 공간에 복사/이동.
-		memcpy(newBlock, data, sizeof(T) * capacity);
+		if (data)
+		{
+			memcpy(newBlock, data, sizeof(T) * capacity);
+		}
 		
 		// 3. 기존 배열 공간 해제.
 		delete[] data;
@@ -56,7 +157,7 @@ public:
 		// 값 추가.
 		data[size++] = value;
 	}
-
+	
 	// R-value-Reference에 대한 insert.
 	void Insert(T&& value)
 	{
@@ -70,7 +171,7 @@ public:
 		// 값을 저장할 때 이동 처리.
 		data[size++] = std::move(value);
 	}
-
+	
 	// index operator overloading.
 	T& operator[](int index)
 	{
@@ -82,6 +183,23 @@ public:
 	// Gettter.
 	int Size() const { return size; }
 	int Capacity() const { return capacity; }
+
+	// 범위 기반 loop 처리를 위한 함수 작성(begin/end).
+	// 배열의 첫 위치를 반환.
+	//Iterator begin()
+	T* begin()
+	{
+		//return Iterator(data);
+		return data;
+	}
+
+	// 배열에 저장된 마지막 요소의 다음 위치를 반환.
+	//Iterator end()
+	T* end()
+	{
+		//return Iterator(data + size);
+		return data + size;
+	}
 
 private:
 	// Heap segement에 할당되는 배열을 관리할 pointer 변수.
--- 2.List/Main.cpp
+++ 2.List/Main.cpp
@@ -9,5 +9,26 @@ int main()
 		list.Insert(ix + 1);
 	}
 
+	// Range-based loop.
+	// 아래 구문은 compiler 내부에서 pointer 연산을 자동으로 하는 것.
+	std::cout << "Range-based loop.\n";
+	for (const int item : list)
+	{
+		std::cout << item << " ";
+	}
+	std::cout << "\n\n";
+
+
+	// loop - Iterator.
+	//std::cout << "Range-based loop using Iterator.\n";
+	//for (
+	//	List<int>::Iterator it = list.begin();
+	//	it != list.end();
+	//	++it
+	//	)
+	//{
+	//	std::cout << *it << " ";
+	//}
+
 	return 0;
 }
